9551cb8716a3b9d9a96565235619c62d02d8c31b
niklas-hjelm <niklas.hjelm@iths.se>
2024-02-07 09:52:45 +0100

    new

---
+++ script.js
@@ -0,0 +1,8 @@
+const header1 = document.getElementById("header1");
+
+console.log(header1.innerText);
+header1.innerText = "Niklas";
+
+const paragraph = document.getElementById("paragraph");
+console.log(paragraph.innerText);
+paragraph.innerText = "Lorem..";
