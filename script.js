bc556c0f323b0b89adf8bdc0fa2fd8642b65c14d
niklas-hjelm <niklas.hjelm@iths.se>
2024-02-07 10:34:20 +0100

    övning 2

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ header1.innerText = "Niklas";
 const paragraph = document.getElementById("paragraph");
 console.log(paragraph.innerText);
 paragraph.innerText = "Lorem..";
+
+const foot = document.getElementById("foot");
+
+console.log(foot.innerHTML);
+
+foot.innerHTML = `
+<ul>
+    <li><a href="http://google.com">Google</a></li>
+    <li><a href="http://w3schools.com">W3Schools</a></li>
+    <li><a href="http://youtube.com">YouTube</a></li>
+</ul>
+`;
